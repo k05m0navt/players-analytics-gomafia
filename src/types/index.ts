d4e322876aac53fdb40796c476c0532f93f10c54
k05m0navt <d.gubajdullin@innopolis.university>
2025-02-12 18:04:43 +0300

    Define types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface PlayerData {
+  name: string;
+  games: number;
+  wins: number;
+  losses: number;
+  winRate: number;
+  blackWins: number;
+  redWins: number;
+  sheriffWins: number;
+  donWins: number;
+  mafiaWins: number;
+  civilianWins: number;
+}
